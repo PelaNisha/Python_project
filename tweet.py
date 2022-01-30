d87e164924eadca773c3c48510db257de6a3887b
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-01-30 12:14:21 +0545

    using request library

--- tweet.py
+++ tweet.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 
+#custom header
 headers = {"Authorization": "Bearer YOUR TOKEN HERE "}
 
 response = requests.get('https://api.twitter.com/2/spaces/search?query=hello&space.fields=title,host_ids', headers=headers)
