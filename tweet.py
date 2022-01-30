3d801245ecb4159f122a7fda14fa4a80d2de5f27
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-01-30 12:13:14 +0545

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,7 +1,7 @@
 import requests
 from pprint import pprint
 
-headers = {"Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAACVqYgEAAAAAAMiRdOfcwm2QtKD1GBxBBhSmN1I%3D0nqn4wxKzg2vERilnjEo0tXCt2oRcGeJAA8yBMsbi7RY0scI3N"}
+headers = {"Authorization": "Bearer YOUR TOKEN HERE "}
 
 response = requests.get('https://api.twitter.com/2/spaces/search?query=hello&space.fields=title,host_ids', headers=headers)
 
