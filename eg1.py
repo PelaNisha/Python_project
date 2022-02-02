c5ad027930a206ffa032c8df3332f44262eff706
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-02-02 11:33:38 +0545

    Scraping

--- eg1.py
+++ eg1.py
@@ -9,52 +9,46 @@ def search(URL, a):
     try:
         response= requests.get(url, timeout=timeout)
         data = response.json()
-        return {'type': 'success', 'data': data}
+        # return {'type': 'success', 'data': data}
+        return data
 
     except requests.exceptions.RequestException as e:  
         return {'type': 'error', 'error': 'Unknown error occured'}
 
-def search_item(arg):
+def search_item(arg, i):
     item = arg+".json"
     if os.path.isfile(item):
         with open(item, 'r') as f:
             data = json.loads(f.read())
             return {'type': 'success', 'data': data}
     else:
-     return append_json(arg)   
+     return append_json(arg,i)   
 
-def parsh_json(response,a):
+def parsh_json(response):
     lst=[]
     for item in response["data"]:
-        for inde in a:
-            char ={
-                a['{inde}']:item[inde],
-                # 'pub-data': item['publishOn'],
-                # 'author':item['author']['name'],
-            }
+        char ={
+            'title': item['title'],
+            'pub-data': item['publishOn'],
+            'author':item['author']['name'],
+        }
         lst.append(char)
     return lst
 
-def append_json(a):
+def append_json(a, i):
     mainList = []
-    b = inp()
-    for n in range (1,4):    
+    for n in range(1,int(i)+1):    
         url = "https://bg.annapurnapost.com/api/tags/news?page="+str(n)+"&per_page=20&tag="
         result = search(url,a)
         with open(a+".json", "w+") as f:
-            mainList.extend(parsh_json(result,b))
+            mainList.extend(parsh_json(result))
             json.dump(mainList, f, indent = 4)
     return {'type': 'success', 'data': mainList}      
 
 
-def inp():
-    a = []
-    number = input("enter the number of items you want to get info")
-    for i in range(1,int(number)):
-        ele = input()
-        a.append(ele)
-    return a   
-
 argument = input("Enter the search item\n")
-p =search_item(argument)
+i = input("Enter the number of pages\n")#added page input for user
+
+
+p =search_item(argument,i)
 print(p)
