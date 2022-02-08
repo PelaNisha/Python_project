e0f85840c15869e0246d1bd8400688e04f9de211
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-02-08 11:35:36 +0545

    scraping trustpilot for customer review filtering

---
+++ customResponse.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from pprint import pprint
+url = "https://www.trustpilot.com/_next/data/categoriespages-consumersite-1638/categories/bank.json?page="
+
+mst =[]
+
+def search(finUrl):
+	timeout = 5
+	res= requests.get(finUrl, timeout=timeout)
+	data = res.json()	
+	return data
+
+			
+def fs():
+	for i in range(2,6):
+		final_url = url + str(i)+"&categoryId=bank"
+		dat = search(final_url)
+		for k in  dat['pageProps']['businesses']['allBusinessUnits']['businessUnits']:
+			char = {
+				'name':k['displayName'],
+				'trustScore':k['trustScore'],
+				'numberOfReviews':k['numberOfReviews'],
+				'stars':k['stars'],
+			}
+			mst.append(char)
+	return mst
+fin = fs()
+pprint(fin) 
