ff0d061c0cb8b0599be58e9e18bbc99705431000
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-03-03 21:12:37 +0545

    Add files via upload

---
+++ urlComment.py
@@ -0,0 +1,55 @@
+import praw
+import json
+from pprint import pprint
+import time
+import re #regular expression
+
+id = "_A9_qXJrlNxXExhn-ZK8RQ"
+secret = "VD0ySjDluZgk74q00tGe_HX-whWZwg"
+ps= "songjoongki"
+ua = "first_app"
+
+
+
+name = "__special"
+
+reddit = praw.Reddit(client_id = id, client_secret = secret, user_agent= ua, username= name, password = ps)
+topic = 'withyoualways'
+
+item= ['https://','www.']
+
+def search_for():
+	lst=[]
+	red = reddit.subreddit('withyoualways')
+	for i in red.hot(limit=3):
+		char ={
+			'title':i.title,
+			'url':i.url,
+		}
+		lst.append(char)
+	return lst	
+
+def url_find():
+	subred = reddit.subreddit(topic)
+	for submission in subred.hot(limit = 3):
+		# submission.upvote()
+		for comment in submission.comments:
+			if hasattr(comment, "body"):
+				comment_lower = comment.body.lower()
+				# print(comment_lower)
+				if Find(comment_lower):
+					# print(submission.url)
+					print(submission.url)
+					comment.reply("Sastodeal\n\nSastodeal is an ecommerce site....\n\nContact: +977142343242\n\nEmails: contact@sastodeal.com\n\nSocial: facebook.com/sastodeal instagram.com/sastodeal\n\nMore: https:/index.plugin.builders/api/site/?url=sastodeal")
+					time.sleep(660)
+				else:
+					pass	
+
+def Find(string):
+  
+	# findall() has been used to match string to url format
+	regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+	url = re.findall(regex,string)      
+	return [x[0] for x in url]
+
+url_find()
