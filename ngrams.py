696e58edf98e66571ac44c24c9c486dcc5f1be8b
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-03-22 15:06:41 +0545

    Update ngrams.py

--- ngrams.py
+++ ngrams.py
@@ -1,13 +1,19 @@
+"""Program to scrape the comments and urls from a subreddit using 
+reddit bot and python to extract the keywords and keyword prase
+"""
+
+# modules used
 from collections import Counter
+from distutils import filelist
 from nltk.corpus import stopwords 	
 import nltk
 import praw
 import os
 import json
-import re
+import re 
 from urllib.parse import urlparse
 
-
+# bot id and other credentials
 id = ""
 secret = ""
 ps= ""
@@ -21,18 +27,18 @@ reddit = praw.Reddit(client_id = id,client_secret = secret,
 
 stop_words = set(stopwords.words('english'))
 
-
-def collectUrlComt(topic):							
+# function that collects url and comments from subreddit into list and string resp.
+def collect_url_Comment(topic):							
 	comment_lower = ""
 	url = []
-	subred = reddit.subreddit(topic)
+	sub_reddit = reddit.subreddit(topic)
 
-	for submission in subred.hot(limit=5):
-		subUrl = submission.url
-		uPar = urlparse(subUrl)
-		hostNm = uPar.hostname
-		if hostNm not in url:
-			url.append(hostNm)
+	for submission in sub_reddit.hot(limit=5):
+		url_in_post = submission.url
+		parse_url = urlparse(url_in_post)
+		host_name = parse_url.hostname
+		if host_name not in url:
+			url.append(host_name)
 		for word in submission.title.split():
 			if word not in stop_words:
 				word = word.lower()
@@ -41,78 +47,90 @@ def collectUrlComt(topic):
 		for comment in submission.comments[:5]:
 			if hasattr(comment, "body"):
 				for word in comment.body.split():
-					if isUrl(word):
-						wUrlPar = urlparse(word)
-						wUrlNam = wUrlPar.hostname
-						if wUrlNam not in url:
-							url.append(wUrlNam)
+					if is_url(word):
+						url_in_comment = urlparse(word)
+						url_hostname = url_in_comment.hostname
+						if url_hostname not in url:
+							url.append(url_hostname)
 					elif word not in stop_words:
 						word = word.lower()
 						comment_lower = comment_lower+" "+word
 
-	return countCmt(topic, url, comment_lower)
+	return count_keywords(topic, url, comment_lower)
 
 
-def isUrl(string):
-	regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+# to check if a string is a url
+def is_url(string):
+	regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+\
+	[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+\
+	(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 	url = re.findall(regex,string)     
-	return [x[0] for x in url]	
+
+	return [x[0] for x in url]
 
 
-def ifFile(topic):
+# to check is a file is already present for respective subreddit
+def if_file(topic):
 	item= topic+'.json'
 	if os.path.isfile(item):
 		return parse(item)
 	else:
-		return collectUrlComt(topic)   
+		return collect_url_Comment(topic)   
+	
 
-
-def countCmt(topic, u, t):
-	ngrams = list(nltk.ngrams(t.split(), n=2))
+# to count the number of times a phrase was repeated
+def count_keywords(topic, url_list, final_string):
+	ngrams = list(nltk.ngrams(final_string.split(), n=2))
 	ngrams_count = {i : ngrams.count(i) for i in ngrams}
-	return sortCmtPhrse(topic, u, ngrams_count)
+
+	return sort_keywords_count(topic, url_list, ngrams_count)
 
 
-def sortCmtPhrse(c, e, d):	
-	item = c+".json"	
-	worddict = {}		
-	urldict = {}
-	urldict['url'] = e				
-	finalList = []
-	finalList.append(urldict)
-	sort_orders = sorted(d.items(), key=lambda x: x[1], reverse=True)
+# sort the keywords phrase in descending order
+def sort_keywords_count(topic, url_list, keyword_count):	
+	item = topic+".json"	
+	words_dict = {}		
+	url_dict = {}			# to combine the urls from post and comments into a dict
+	url_dict['url'] = url_list				
+	final_list = []
+	final_list.append(url_dict)
+	sort_orders = sorted(keyword_count.items(), key=lambda x: x[1], reverse=True)
 
 	for i in sort_orders[:20]:
-		worddict['words'] = i[0]
-		worddict['count'] = i[1]
-		finalList.append(worddict)
-		worddict = {}
+		words_dict['words'] = i[0]
+		words_dict['count'] = i[1]
+		final_list.append(words_dict)
+		words_dict = {}
 		
 	with open(item, "w+") as f:
-		json.dump(finalList, f, indent = 2)
+		json.dump(final_list, f, indent = 2)
 
-	return finalList
+	return final_list
 
 
+# parse the file for subreddit
 def parse(top):
 	print("File Present!")
 	with open(top, 'r') as f:
 		data = json.load(f)
 	return data
-	
 
-def retUrl(data):
+	
+# call this function to return the urls from the file
+def return_urls(data):
 	return data[0]['url']
 
 
-def retWord(data):
+# call this function to return the keyword phrases from the file
+def return_words(data):
 	for i in range(1, len(data)):
 		print(data[i])
 	return "End of file"	
 
 
-topic = input("Enter the subreddit: ")  
-# a = retUrl(ifFile(topic))
+# input the subreddit from the user
+subreddit = input("Enter the subreddit: ")  
+# a = return_urls(ifFile(topic))
 # print(a)
-result = retWord(ifFile(topic))
-print(result)
+output = return_words(if_file(subreddit))
+print(output)
