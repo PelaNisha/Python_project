01c3155ed36ebc7259f001bf620b997e6c7f0854
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-03-28 23:13:50 +0545

    Update interaction_bot.py

--- interaction_bot.py
+++ interaction_bot.py
@@ -1,9 +1,9 @@
 """Program to scrape the comments and obtain the user interaction on a comment
-about who replied to whom, the comment and its keyword
+about who replied to whom, the comment and its keywords
 """
 
 # modules used
-from test import * # file as a package to impoet functions
+from test import * # file as a package to import functions
 import praw	
 import os
 import json
@@ -25,43 +25,49 @@ reddit = praw.Reddit(client_id = id_,client_secret = secret,
 stop_words = set(stopwords.words('english'))
 
 
-# function that collects url and comments from subreddit into list and string resp.
-def commenters_names(topic):
+# make a list of dicts for comment author, repliers, repliers count and comment body
+def commenters_info_and_comment(topic):
 	final_list = []
 	subred = reddit.subreddit(topic)
 	for submission in subred.hot(limit = 3):
 		for comment in submission.comments[:10]:
 			if hasattr(comment, "body"):
+				original_comment_author = []
 				replied_comment_author = []
 				authors_dict = {}	
-				comment_author = comment.author.name
+				comment_author = comment.author
 				comment_body = comment.body
-				authors_dict['comment author'] = comment_author
-				authors_dict['comment body'] = comment_body
-								
-				for reply in comment.replies:
-					replied_comment_author.append(reply.author.name)
+				if comment_author not in original_comment_author:
+					authors_dict['comment author'] = str(comment_author)
+					authors_dict['comment body'] = str(comment_body)
+					original_comment_author.append(str(comment_author))
 
+				for reply in comment.replies:
+					replied_comment_author.append(str(reply.author))
 				authors_dict['repliers'] = replied_comment_author
 				authors_dict['repliers_count'] = len(replied_comment_author)
 				final_list.append(authors_dict)
-
+				
 	li = sorted(final_list, key = lambda i: i['repliers_count'], reverse=True)
 
-	return second_step(li, topic)
+	return dump_json(li, topic)
 
 
-def second_step(final_list, top):
+# dump the json response and also print the keywords for comments
+def dump_json(final_list, top):
 	with open(top+".json", "w+") as f:
 		json.dump(final_list, f, indent = 2)
-	
+	print("Data Scrapped Successfully!!")
 
-# to check is a file is already present for respective subreddit
+
+# To check is a file is already present for respective subreddit and 
+# Scape the data is file is present
 def if_file(topic):
 	item= topic+'.json'
+	
 	if os.path.isfile(item):
 		data =  parse_(item)
-		str_ = "" # concat the string to make a large a comment 
+		str_ = "" # To concat the string  
 		for i in range(0,len(data)):
 			for word in data[i]['comment body'].split():
 				if word not in stop_words:
@@ -70,7 +76,7 @@ def if_file(topic):
 		x =  count_keywords(str_)
 		pprint(x)
 	else:
-		return commenters_names(topic)  
+		return commenters_info_and_comment(topic) 
 
 
 # Count the number of times a phrase was repeated
@@ -80,19 +86,21 @@ def count_keywords(final_string):
 
 	return sort_keywords_count(ngrams_count)
 
+
 # Sort the keywords phrase in descending order
 def sort_keywords_count(keyword_count):		
-	words_dict = {}					
+	keywords_dict = {}					
 	final_list = []
 	sort_orders = sorted(keyword_count.items(), key=lambda x: x[1], reverse=True)
 
 	for i in sort_orders[:20]:
-		words_dict['words'] = i[0]
-		words_dict['count'] = i[1]
-		final_list.append(words_dict)
-		words_dict = {}
+		keywords_dict['words'] = i[0]
+		keywords_dict['count'] = i[1]
+		final_list.append(keywords_dict)
+		keywords_dict = {}
 	return final_list	
 
 
+# Input the subreddit title
 topic = input("Enter the topic: ")
 if_file(topic)
