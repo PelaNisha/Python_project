b906013f9872d251d2d00cda409d258295e88bac
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-04-12 21:05:07 +0545

    Update auto_tabs.py

--- auto_tabs.py
+++ auto_tabs.py
@@ -1,6 +1,9 @@
+# Python program that takes a url and duration, opens the url in different tabs after a certain duration
+# Uses: Can be used to increase view of a youtube video
+
 import webbrowser, time
 url = input("Enter the url: ")
 duration = input("Enter the duration: ")
-for i in range(2):
+for i in range(2):                      # here 2 is the number of tabs we want to open
     webbrowser.open_new_tab(url)
-    time.sleep(int(duration))
+    time.sleep(int(duration))           # time in between we open the next tab
